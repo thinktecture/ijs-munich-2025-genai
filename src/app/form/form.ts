4d65ac390ab24a9b84af70ff78733af99b053182
Christian Liebel <christian.liebel@thinktecture.com>
2025-10-27 16:01:32 +0100

    Lab #16

--- src/app/form/form.ts
+++ src/app/form/form.ts
@@ -21,10 +21,18 @@ export class Form {
     const languageModel = await LanguageModel.create({
       initialPrompts: [{
         role: 'system',
-        content: `Extract the information to a JSON object of this shape: ${JSON.stringify(this.formGroup.value)}`
+        content: `Extract the information.`
       }]
     });
-    const result = await languageModel.prompt(value);
+    const result = await languageModel.prompt(value, {
+      responseConstraint: {
+        type: 'object',
+        properties: {
+          name: { type: 'string' },
+          city: { type: 'string' }
+        }
+      }
+    });
     console.log(result);
   }
 
